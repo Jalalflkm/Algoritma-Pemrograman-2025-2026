2c201e4c12f320632aa09931f4094077e48d5d5a
Jalalflkm <mhmdjalal2103@gmail.com>
2025-10-27 09:22:00 +0700

    Create globalvariable.java

---
+++ Java/globalvariable.java
@@ -0,0 +1,10 @@
+public class GlobalVariable {
+  public static int age; // global variable
+
+  public static void main(String args[]) {
+    age = 20;
+    System.out.println("Umur Jalal : " + age);
+  }
+}
+
+7.    Buat 
