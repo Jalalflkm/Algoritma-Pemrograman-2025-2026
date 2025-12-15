8e1410f0ac2e3289d73dfc42561fdfd682ccee53
Jalalflkm <mhmdjalal2103@gmail.com>
2025-12-15 14:07:16 +0700

    Create perkenalan.php

---
+++ PHP/perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+// membuat fungsi
+function perkenalan(){
+    echo "Assalamulaikmu, ";
+    echo "Perkenalkan, nama saya jalal<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+// memanggil fungsi yang sudah dibuat
+perkenalan(); // BARIS INI DIPINDAHKAN KE DALAM TAG PHP
+?>
