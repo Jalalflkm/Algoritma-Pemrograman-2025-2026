a11ed4ca9cc7329bf768ca8528c53d1311fdbf4a
Jalalflkm <mhmdjalal2103@gmail.com>
2025-12-15 11:05:32 +0700

    Create Latihan2Prosedur.java

---
+++ Java/Latihan2Prosedur.java
@@ -0,0 +1,17 @@
+public class Latihan2Prosedur
+{
+    public static void main(String[] args) {
+        MetodePoin(255.7);
+    }
+
+    public static void MetodePoin(double poin) {
+        if (poin > 202.5) {
+            System.out.println("--Poin Yang Keluar--");
+            System.out.println("Poin : Pertama");
+        } else if (poin >= 122.4) {
+            System.out.println("Poin : Kedua");
+        } else {
+            System.out.println("Poin : Ketiga");
+        }
+    }
+}
