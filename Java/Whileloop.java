adcd0017a4c961eb9e24b23b9484a4b9292483c2
Jalalflkm <mhmdjalal2103@gmail.com>
2026-01-31 00:33:12 +0700

    Create Whileloop.java

---
+++ Java/Whileloop.java
@@ -0,0 +1,9 @@
+public class Whileloop {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 10) {
+            System.out.println("Variabel a Kurang dari 10 = " + a);
+            a++;
+        }
+    }
+}
