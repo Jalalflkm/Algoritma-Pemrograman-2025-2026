3d2f6b9e7c57751751b6aa728761d6619449ef19
Jalalflkm <mhmdjalal2103@gmail.com>
2026-01-31 00:32:06 +0700

    Create Segitigaterbalik.java

---
+++ Java/Segitigaterbalik.java
@@ -0,0 +1,11 @@
+public class Segitigaterbalik {
+    public static void main(String[] args) {
+        int syarat = 6;
+        for (int i = 1; i <= syarat; i++) {
+            for (int j = syarat; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
