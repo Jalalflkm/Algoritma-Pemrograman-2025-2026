b43a5e4d92bccf3dd7007bfb6e500be280ae61a4
Jalalflkm <mhmdjalal2103@gmail.com>
2026-01-31 00:31:41 +0700

    Create Perulangannilaiarray.java

---
+++ Java/Perulangannilaiarray.java
@@ -0,0 +1,8 @@
+public class Perulangannilaiarray {
+  public static void main(String[] args) {
+    String[] akuSuka = {"Aku", "Suka", "Ngoding"};
+    for (int i = 0; i < akuSuka.length; i++) {
+      System.out.println(akuSuka [i]);
+    }
+  }
+}
