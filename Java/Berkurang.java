2d4d91568454357484521c93dc91766ee3085b11
Jalalflkm <mhmdjalal2103@gmail.com>
2026-01-31 00:28:15 +0700

    Create Berkurang.java

---
+++ Java/Berkurang.java
@@ -0,0 +1,8 @@
+public class Berkurang {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Selamat Menunggu Tahun Baru 2023");
+    }
+}
