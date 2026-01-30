2fb8330bccda9ffcacb936d4bc2ca340b6af04a5
Jalalflkm <mhmdjalal2103@gmail.com>
2026-01-31 00:33:41 +0700

    Delete fungsi-parameter.py

--- Pythom/fungsi-parameter.py
+++
@@ -1,8 +0,0 @@
-# Membuat fungsi dengan parameter
-def luas_segitiga(alas, tinggi):
-  luas = (alas * tinggi) / 2
-  print ('Luas segitiga: %f' % luas) 
-  # Pemanggilan fungsi
-  luas_segitiga(4, 6)
-
-
