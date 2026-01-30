53b323b7ffa7a23cd23962972f96a07642c5b7be
Jalalflkm <mhmdjalal2103@gmail.com>
2026-01-31 00:28:05 +0700

    Create Arraykata.java

---
+++ Java/Arraykata.java
@@ -0,0 +1,9 @@
+public class Arraykata {
+    public static void main(String[] args) {
+        String[] teman = {"Tono", "Tini", "Ani", "Ana", "Dina"};
+
+        for (int i = 0; i < teman.length; i++) {
+            System.out.println(teman[i]);
+        }
+    }
+}
